d8d8380c58b22cde734fafeff235f4553671f27f
Hassan Zouhar <43142217+hassanzouhar@users.noreply.github.com>
2025-07-03 00:07:19 +0200

    Set up complete React project structure with TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MonotropismQuestionnaire from './monotropism-questionnaire';
+
+function App() {
+  return (
+    <div className="App">
+      <MonotropismQuestionnaire />
+    </div>
+  );
+}
+
+export default App;
